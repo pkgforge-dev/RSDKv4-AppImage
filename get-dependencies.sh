916ce54720fd6702ec487899f708463c21948c3f
Link4Electronics <link4electronics@gmail.com>
2026-08-15 03:57:42 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,13 +10,26 @@ pacman -Syu --noconfirm \
     cmake    \
     glew     \
     libdecor \
-    sdl2
+    sdl2     \
+    tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package rsdkv4-git
+#make-aur-package rsdkv4-git
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of RSDKv4 Decompilation..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/RSDKModding/RSDKv4-Decompilation"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./RSDKv4
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./RSDKv4
+cmake -S ./ -B build -D CMAKE_BUILD_TYPE=Release -D USE_SDL_AUDIO=ON
+cmake --build build -j$(nproc)
+mv -v ./build/RSDKv4 ../AppDir/bin
