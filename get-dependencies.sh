e290f8c56840ff73372cb375075b8bc405983fcc
Link4Electronics <link4electronics@gmail.com>
2026-08-15 03:57:42 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,17 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    cmake    \
-    glew     \
-    libdecor \
-    sdl2
+pacman -Syu --noconfirm cmake glew sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package rsdkv4-git
+echo "Building RSDKv4 Decompilation..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/RSDKModding/RSDKv4-Decompilation"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./RSDKv4
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+cmake -S ./RSDKv4 -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j$(nproc)
+mv -v ./build/RSDKv4 ./AppDir/bin
