27aeb1469c0b4d3e71e9f583978d38dc22d43b4b
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:52:38 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,7 +12,5 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun ./AppDir/bin/RSDKv4
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
